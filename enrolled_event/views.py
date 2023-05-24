6f7ac42abd8995ba56ea9b588a29fcc2f4ffea65
Amanda Christie Tarigan <mandachristie04@gmail.com>
2023-05-24 18:08:53 +0700

    try query

--- enrolled_event/views.py
+++ enrolled_event/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import *
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from utils.query import *
+from datetime import datetime
+
 
 # Create your views here.
-def show_enrolled_event(request):
-    return render(request, "enrolled_event.html")
+#def show_enrolled_event(request):
+cursor.execute(f""" 
+    SELECT * from peserta_mendaftar_event;
+""")
+
+data_event_didaftar = cursor.fetchall()
+    #final_data_kategori_resto = []
+'''
+for cat in data_kategori_resto:
+    cursor.execute(
+        f'SELECT * from RESTAURANT_CATEGORY, RESTAURANT R where Rcategory = \'{cat[0]}\''
+    )
+    canDelete = (len(cursor.fetchall()) == 0)
+    tuple_new = cat + (canDelete, )
+    final_data_kategori_resto.append(tuple_new)
+'''
+
+print(data_event_didaftar)
+
+'''
+context = {
+    'list_kategori_resto': final_data_kategori_resto,
+    'role':request.COOKIES.get('role'),
+}
+return render(request, "enrolled_event.html", context)
+'''
+
--- project_django/settings.py
+++ project_django/settings.py
@@ -151,3 +151,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
