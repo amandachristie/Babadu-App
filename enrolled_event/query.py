42d1dd77e82369f7ea5009b0748cc15cef2bc63a
Amanda Christie Tarigan <mandachristie04@gmail.com>
2023-05-25 09:59:22 +0700

    allow origin

--- enrolled_event/query.py
+++ enrolled_event/query.py
@@ -1,4 +1,5 @@
 #SQL QUERY
+from sqlite3 import InternalError
 from uuid import UUID
 from django.db import connection
 
@@ -8,7 +9,7 @@ def parse(cursor):
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
 def SQLenrolledEvent(nama):
-    no_peserta = findNoPeserta(nama)
+    no_peserta = find_no_peserta(nama)
     query =  f'''SELECT *
         FROM peserta_mendaftar_event P, event E 
         WHERE P.nama_event = E.nama_event AND P.tahun = E.tahun AND P.nomor_peserta = {no_peserta[0]};
@@ -20,7 +21,7 @@ def SQLenrolledEvent(nama):
     res = parse(cursor)
     return res
 
-def findId(nama):
+def find_id(nama):
     query =  f'''SELECT ID
         FROM MEMBER
         WHERE Nama = '{nama}';
@@ -33,8 +34,8 @@ def findId(nama):
     print(id_atlet)
     return id_atlet[0]
 
-def findNoPeserta(nama):
-    id_atlet = findId(nama)
+def find_no_peserta(nama):
+    id_atlet = find_id(nama)
     print(id_atlet)
     query =  f'''SELECT nomor_peserta
         FROM PESERTA_KOMPETISI
@@ -46,3 +47,21 @@ def findNoPeserta(nama):
     cursor.execute(query)
     no_peserta = cursor.fetchall()
     return no_peserta[0]
+
+def delete(nama_atlet, nama_event, tahun_event):
+    no_peserta = find_no_peserta(nama_atlet)
+
+    try:
+        query = f'''DELETE FROM PESERTA_MENDAFTAR_EVENT
+            WHERE nomor_peserta = {no_peserta[0]} AND nama_event = '{nama_event}' AND tahun = '{tahun_event}';
+            '''
+        cursor = connection.cursor()
+        cursor.execute("set search_path to babadu;")
+        cursor.execute(query)
+        connection.commit()
+        connection.close()
+        
+    except InternalError as e:
+        error_message = str(e)
+
+    return error_message
--- enrolled_event/views.py
+++ enrolled_event/views.py
@@ -1,3 +1,4 @@
+from sqlite3 import InternalError
 from django.shortcuts import *
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
@@ -15,6 +16,18 @@ def show_enrolled_event(request):
     }
     return render(request, "enrolled_event.html", context)
 
-def delete_event(request):
-    print('delete')
-    return 1
+def delete_event(request, nama_event, tahun_event):
+    print(nama_event)
+    print(tahun_event)
+
+    result = delete((request.session['user']['nama']), nama_event, tahun_event)
+    if result != None:
+        data_enrolled_event = SQLenrolledEvent(request.session['user']['nama'])
+        context = {
+            'data_enrolled_event' : data_enrolled_event,
+            'error_message': result
+        }
+        return render(request, 'enrolled_event.html', context)
+    
+    show_enrolled_event()
+    
